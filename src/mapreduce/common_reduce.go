41d7f342149d1ea00e622b421d485196cca10196
Hoanh An <hoanhan101@gmail.com>
2018-03-04 21:55:57 -0500

    clean up common reduce

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -18,35 +18,6 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	//
-	// Your doMap() encoded the key/value pairs in the intermediate
-	// files, so you will need to decode them. If you used JSON, you can
-	// read and decode by creating a decoder and repeatedly calling
-	// .Decode(&kv) on it until it returns an error.
-	//
-	// You may find the first example in the golang sort package
-	// documentation useful.
-	//
-	// reduceF() is the application's reduce function. You should
-	// call it once per distinct key, with a slice of all the values
-	// for that key. reduceF() returns the reduced value for that key.
-	//
-	// You should write the reduce output as JSON encoded KeyValue
-	// objects to the file name outputFile. We require you to use JSON
-	// because that is what the merger than combines the output
-	// from all the reduce tasks expects. There is nothing special about
-	// JSON -- it is just the marshalling format we chose to use. Your
-	// output code will look something like this:
-	//
-	// enc := json.NewEncoder(file)
-	// for key := ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
-	// Your code here (Part I).
-	//
-
     // Make a decoder slice with the number of map tasks.
 	var decoders = make([]*json.Decoder, nMap)
 
