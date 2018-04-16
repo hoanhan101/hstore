4c6403c21582070eb2ada415185f33a71b78ca77
Hoanh An <hoanhan101@gmail.com>
2018-04-16 10:07:37 -0400

    add usage and example

--- run_6824.sh
+++ run_6824.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-# run_6824.sh - Run MIT's 6.824 tests 
+# run_6824.sh - Run MIT's 6.824 tests for a specific package
 # Author: Hoanh An (hoanhan@bennington.edu)
 # Date: 04/15/18
+#
+# Usage:
+#   bash run_6824.sh <package_name>
+#
+# Example:
+#   bash run_6824.sh kvraft will runs tests for kvraft package
 
 export "GOPATH=$PWD"
 cd "$GOPATH/src/$1"
